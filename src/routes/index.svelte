f674316981a26d0ffec4619e13cc0797b5d21ee1
benpaquier <hello@benpaquier.com>
2022-08-05 10:57:05 +0200

    Services

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,10 +1,12 @@
 <script>
 	import Layout from '$lib/Layout.svelte'
 	import Hero from '../lib/Hero.svelte'
+	import Services from '../lib/Services.svelte'
 	import Teaching from '../lib/Teaching.svelte'
 </script>
 
 <Layout>
 	<Hero />
 	<Teaching />
+	<Services />
 </Layout>
